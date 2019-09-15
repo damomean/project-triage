80b0ca38194a5251f2e209384aaf79b07508eded
Damone Means <dukemeans@gmail.com>
2019-09-14 21:53:51 -0400

    in class changes

--- app/public/dummy.php
+++ app/public/dummy.php
@@ -4,20 +4,23 @@
 		"firstName": "Chuck",
 		"lastName": "Norris",
 		"dob": "1940-03-10",
-		"sexAtBirth": "M"
+		"sexAtBirth": "M",
+		"priority": "high"
 	},
 	{
 		"patientGuid": "BITE-CHEW-SWALLOW-2019",
 		"firstName": "Bear",
 		"lastName": "Grylls",
 		"dob": "1989-07-21",
-		"sexAtBirth": "M"
+		"sexAtBirth": "M",
+		"priority": "high"
 	},
 	{
 		"patientGuid": "DROP-OUT-HS001",
 		"firstName": "Didi",
 		"lastName": "Conn",
 		"dob": "1951",
-		"sexAtBirth": "W"
+		"sexAtBirth": "W",
+		"priority": "low"
 	}
 ]
